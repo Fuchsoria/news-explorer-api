69362914debfe893a66a80e90cd820c435f19084
German <sehenqq@gmail.com>
2020-02-19 20:16:05 +0100

    Update allowedDomains

--- configuration/config.js
+++ configuration/config.js
@@ -4,7 +4,7 @@ const {
   NODE_ENV, JWT_SECRET, DB_LINK, PORT,
 } = process.env;
 
-const allowedDomains = ['http://localhost:8080', 'https://fuchsoria.github.io', 'http://news-explorer.info', 'https://news-explorer.info', 'http://test.news-explorer.info:8080', 'https://news-explorer.fuchsoria.dev'];
+const allowedDomains = ['http://localhost:8080', 'https://fuchsoria.github.io', 'http://news-explorer.info', 'https://news-explorer.info', 'http://test.news-explorer.info:8080', 'https://news-explorer.fuchsoria.dev', 'http://test.fuchsoria.dev'];
 
 const devSecret = 'dev-secret';
 const devDbLink = 'mongodb://localhost:27017/newsdb';
